4f1f129bca9fbbc210bbf416e0cdc79e9c148315
Juan Jose T Montana <juanjosetmontana@gmail.com>
2022-11-28 22:25:44 -0800

    Delete and Search added to the SIngly Linked List

--- linked_lists.py
+++ linked_lists.py
@@ -27,6 +27,10 @@ class SinglyLinkedList:
         return str(self.llsize)
 
     def iter(self):
+        '''
+        Traverse the list if a head exists and until the next pointer  is None, it uses yield so remember
+        to instanciate the iterator and use next on it
+        '''
         current = self.head
 
         while current:
@@ -35,21 +39,39 @@ class SinglyLinkedList:
             yield val
 
     def delete(self, data):
-        current = self.tail
-        previous = self.tail
+        '''
+        Delete the node searching the value while traversing the list.
+        '''
+        current = self.head
+        previous = self.head
 
         while current:
             if current.data == data:
-                if current == self.tail:
-                    self.tail = current.next
+                if current == self.head:
+                    self.head = current.next
                 else:
                     previous.next = current.next
                     self.size -= 1
                     #return deleted value
                     return current.data
             
+            #Move to the next node to try to find the node to be deleted
             previous = current
             current = current.next
+
+    
+    def search(self,data):
+        '''YOu can either return  the node when you find it or just print a message that 
+        it exists.
+        '''
+        # x = self.head
+        # while x!=None and x.data != data:
+        #     x = x.next
+
+        # return x
+        for node in self.iter():
+            if data == node:
+                print(f'Data {data} found!')
 
 
 if __name__=='__main__':
@@ -60,7 +82,7 @@ if __name__=='__main__':
     LL.push(1)
     LL.push(5)
     LL.push(8)
-    print(LL.head.next.data)
+    #print(LL.head.next.data)
     
     
     my_iter = LL.iter()
@@ -68,7 +90,9 @@ if __name__=='__main__':
     print(next(my_iter))
     print(next(my_iter))
 
-    print(LL.size())
+    print(f'Size of the LL: {LL.size()}')
+    
+    LL.search(8)
     
 
 
