d3b33e5c41386d6a9316ee8f5a2a19eeff1e4121
Juan Jose T Montana <juanjosetmontana@gmail.com>
2022-11-28 21:10:47 -0800

    Nodes Creation and SInlgy Linked List w/ push and size methods

--- linked_lists.py
+++ linked_lists.py
@@ -1,26 +1,75 @@
 
-class Node:
+from nodes_creation import Node
 
-    def __init__(self, data, next=None) -> None:
-        self.data = data
-        self.next = next
+class SinglyLinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.llsize = 0
+
+    def push(self, data):
+        node = Node(data)
+
+        #Check if your LL has Nodes already if not  it starts assigning a head
+        if self.head == None:
+            self.head = node
+        else:
+            current = self.head
+
+            #Adding the object at the end of the LL
+            while current.next:
+                current = current.next
+
+            current.next = node
         
+        self.llsize += 1
+
+    def size(self):
+        return str(self.llsize)
+
+    def iter(self):
+        current = self.head
+
+        while current:
+            val = current.data
+            current = current.next
+            yield val
+
+    def delete(self, data):
+        current = self.tail
+        previous = self.tail
+
+        while current:
+            if current.data == data:
+                if current == self.tail:
+                    self.tail = current.next
+                else:
+                    previous.next = current.next
+                    self.size -= 1
+                    #return deleted value
+                    return current.data
+            
+            previous = current
+            current = current.next
 
 
 if __name__=='__main__':
-    
-    node2 = Node("A", None)
-    node3 = Node("B", node2)
-    node1 = Node("C", node3)
-    print(f'Node in node 2: {node2.data}')
-    print(f'Node after node3: {node3.next.data}')
-    print(f'Node after node1: {node1.next.data}')
 
-    #Creating a linked list
-    head = None
-    for count in range(1, 5):
-        head = Node(count, head)
+    LL = SinglyLinkedList()
     
-    while head != None:
-        print(head.data)
-        head = head.next
+    
+    LL.push(1)
+    LL.push(5)
+    LL.push(8)
+    print(LL.head.next.data)
+    
+    
+    my_iter = LL.iter()
+    print(next(my_iter))
+    print(next(my_iter))
+    print(next(my_iter))
+
+    print(LL.size())
+    
+
+
+                
---
+++ nodes_creation.py
@@ -0,0 +1,27 @@
+
+class Node:
+
+    def __init__(self, data, prev=None, next=None) -> None:
+        self.data = data
+        self.next = next
+        self.prev = prev
+        
+
+
+if __name__=='__main__':
+    
+    node2 = Node("A", None)
+    node3 = Node("B", node2)
+    node1 = Node("C", node3)
+    print(f'Node in node 2: {node2.data}')
+    print(f'Node after node3: {node3.next.data}')
+    print(f'Node after node1: {node1.next.data}')
+
+    #Creating a linked list
+    head = None
+    for count in range(1, 5):
+        head = Node(count, head)
+    
+    while head != None:
+        print(head.data)
+        head = head.next
